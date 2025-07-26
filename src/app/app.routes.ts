9119a3c2df83067e761e3f46ec48e935b111c2a7
Leandro de Oliveira Torres Santos <leanots8@gmail.com>
2025-07-26 20:11:53 -0300

    Criando componente Sobre, e criando uma rota para ele em Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../app/pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'sobre', component: SobreComponent }, // Substitua por outro componente se necessário
 ];
