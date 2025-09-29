558c433f0b6ac940bd3350b1e3b98a1d50965cf8
Leandro de Oliveira Torres Santos <leanots8@gmail.com>
2025-09-29 03:30:26 -0300

    Alteração do environment e adição de correta e classe de ativos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
 
-    apiUrl: 'http://localhost:8081',
-    apiUrlTransacoes: 'http://localhost:8081/transacao/buscarPorTicket'
+    apiUrl: 'http://localhost:8083',
+    apiUrlTransacoes: 'http://localhost:8083/transacao/buscarPorTicket'
 
 };
