7322170a4235307a4245a3f6c047de5196a9d012
Leandro de Oliveira Torres Santos <leanots8@gmail.com>
2026-02-17 14:17:13 -0300

    feat: ajuste para deploy no Render

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,38 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'excluir-transacao/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'editar-transacao/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'home',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'busca-ativo',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'investimento-form',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'busca-ativo-consolidado',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'upload-dividendos',
+    renderMode: RenderMode.Prerender
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
