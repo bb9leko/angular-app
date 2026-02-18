857064a271404e6dd63635404210a15d6ba91c37
Leandro de Oliveira Torres Santos <leanots8@gmail.com>
2026-02-17 23:46:06 -0300

    Configurando url do render da api-consumer em envirionments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
 
-    apiUrl: 'http://localhost:8083',
+    apiUrl: 'https://api-consumer-quarkus.onrender.com',
     apiUrlDividendos: 'http://localhost:8081',
     apiUrlTransacoes: 'http://localhost:8083/transacao/buscarPorTicket',
     apiUrlTransacoesConsolidado: 'http://localhost:8081/ativos/consolidados'
