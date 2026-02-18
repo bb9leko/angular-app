fac6425584a205a99e0e7c2ca8c8a89d7911c6c6
Leandro de Oliveira Torres Santos <leanots8@gmail.com>
2026-02-17 23:54:54 -0300

    Ajustes para deploy no Render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
 
     apiUrl: 'https://api-consumer-quarkus.onrender.com',
     apiUrlDividendos: 'http://localhost:8081',
-    apiUrlTransacoes: 'http://localhost:8083/transacao/buscarPorTicket',
+    apiUrlTransacoes: 'https://api-consumer-quarkus.onrender.com/transacao/buscarPorTicket',
     apiUrlTransacoesConsolidado: 'http://localhost:8081/ativos/consolidados'
 
 };
